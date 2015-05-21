ddb9c5126ff97e63bf0ad59acff58aa15eb2e108
ajtomato <ajtomato@gmail.com>
2015-05-21 17:56:52 +0800

    Move the test code for console input & output to a separated function.

--- HelloWorld/HelloWorld/Program.cs
+++ HelloWorld/HelloWorld/Program.cs
@@ -7,22 +7,26 @@ namespace HelloWorld
 {
     class Program
     {
+        static void ConsoleInputOutput()
+        {
+            // The full command.
+            System.Console.WriteLine(System.Environment.CommandLine);
+
+            // Input
+            System.Console.Write("Please input something: ");
+            String inputStr = System.Console.ReadLine();
+            System.Console.WriteLine("The input string is {0}", inputStr);
+        }
+
         static int Main(string[] args)
         {
-            System.Console.WriteLine("Hello world");
-
             // Only the command line arguments are included.
             foreach (string arg in args)
             {
                 System.Console.WriteLine(arg);
             }
 
-            // The full command.
-            System.Console.WriteLine(System.Environment.CommandLine);
-
-            System.Console.Write("Please input something: ");
-            String inputStr = System.Console.ReadLine();
-            System.Console.WriteLine("The input string is {0}", inputStr);
+            ConsoleInputOutput();
 
             return 0;
         }
