4a4972c947be5315e549091bd704ef63b129492a
ajtomato <ajtomato@gmail.com>
2015-05-26 08:37:09 +0800

    Read a keystroke.

--- HelloWorld/HelloWorld/Program.cs
+++ HelloWorld/HelloWorld/Program.cs
@@ -9,17 +9,22 @@ namespace HelloWorld
     {
         static void ConsoleInputOutput()
         {
-            // The full command.
-            System.Console.WriteLine(System.Environment.CommandLine);
-
-            // Input
+            // Input a line.
             System.Console.Write("Please input something: ");
             String inputStr = System.Console.ReadLine();
             System.Console.WriteLine("The input string is {0}", inputStr);
+
+            // Input a character. System.Console.Read() is NOT preferable.
+            System.Console.Write("Please input something again: ");
+            System.ConsoleKeyInfo inputKey = System.Console.ReadKey();
+            System.Console.WriteLine(inputKey.Key.ToString());
         }
 
         static int Main(string[] args)
         {
+            // The full command.
+            System.Console.WriteLine(System.Environment.CommandLine);
+
             // Only the command line arguments are included.
             foreach (string arg in args)
             {
