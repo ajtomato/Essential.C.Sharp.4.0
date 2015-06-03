1bdc05015c6d1cf6f47bac44049e82708759985e
ajtomato <ajtomato@gmail.com>
2015-06-03 17:50:36 +0800

    Clean compile warnings.

--- HelloWorld/HelloWorld/Program.cs
+++ HelloWorld/HelloWorld/Program.cs
@@ -117,7 +117,7 @@ namespace HelloWorld
             }
             catch (Exception e)
             {
-                System.Console.WriteLine("Parse error exception is caught.");
+                System.Console.WriteLine("Parse error exception is caught: " + e.ToString());
             }
 
             try
@@ -131,7 +131,7 @@ namespace HelloWorld
             }
             catch (Exception e)
             {
-                System.Console.WriteLine("Parse error exception is caught.");
+                System.Console.WriteLine("Parse error exception is caught: " + e.ToString());
             }
         }
 
